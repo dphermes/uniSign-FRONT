6337150b4010b2588d308e6e9ac6ec8cfe65c0cc
damienpuaud <damien.puaud@gmail.com>
2022-01-26 10:33:20 +0100

    CREATE - Registering User HTTP Call

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -13,8 +13,23 @@ export class AuthenticationService {
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * Login http service call
+   * @param user User: Whole user object
+   */
   public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
     // Observe the whole response instead of the body to fetch the headers with the Jwt Token
     return this.http.post<HttpResponse<any> | HttpErrorResponse>(`${this.host}/user/login`, user, {observe: 'response'});
   }
+
+  /**
+   * Register http service call
+   * @param user User: Whole user object
+   */
+  public register(user: User): Observable<User | HttpErrorResponse> {
+    // Observe the whole response instead of the body to fetch the headers with the Jwt Token
+    return this.http.post<User | HttpErrorResponse>(`${this.host}/user/register`, user, {observe: 'response'});
+  }
+
+
 }
