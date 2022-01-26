db58ed4582ccd5dfb17f49986d0ed05e9a4114f2
damienpuaud <damien.puaud@gmail.com>
2022-01-26 10:23:41 +0100

    CREATE - Login Http Service Call to backend

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { environment } from "../../environments/environment";
+import {HttpClient, HttpErrorResponse, HttpResponse} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  private host = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
+    // Observe the whole response instead of the body to fetch the headers with the Jwt Token
+    return this.http.post<HttpResponse<any> | HttpErrorResponse>(`${this.host}/user/login`, user, {observe: 'response'});
+  }
 }
