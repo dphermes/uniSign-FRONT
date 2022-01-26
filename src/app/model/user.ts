a800cbaa337b36148daf9f598804cb94d6a31b63
damienpuaud <damien.puaud@gmail.com>
2022-01-26 12:18:03 +0100

    CREATE - Add properties in User Class

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,3 +1,32 @@
 export class User {
+  private id: number;
+  private userId: string;
+  private firstName: string;
+  private lastName: string;
+  private username: string;
+  private email: string;
+  private loginDateDisplay: Date;
+  private joinDate: Date;
+  private profileImageUrl: string;
+  private isActive: boolean;
+  private isNotLocked: boolean;
+  private role: string;
+  private authorities: string[];
 
+
+  constructor() {
+    this.id = 0;
+    this.userId = '';
+    this.firstName = '';
+    this.lastName = '';
+    this.username = '';
+    this.email = '';
+    this.loginDateDisplay = new Date();
+    this.joinDate = new Date();
+    this.profileImageUrl = '';
+    this.isActive = false;
+    this.isNotLocked = false;
+    this.role = '';
+    this.authorities = [];
+  }
 }
