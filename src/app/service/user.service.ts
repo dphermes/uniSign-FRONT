339b93ae40ae249f27546636212dc20bfd3db2b8
damienpuaud <damien.puaud@gmail.com>
2022-01-26 11:36:54 +0100

    CREATE - Fetch all users service (http call)

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { environment } from "../../environments/environment";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../model/user";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private host = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Fetch all users from API
+   * @return User[]: list of all users
+   */
+  public getUsers(): Observable<User[] | HttpErrorResponse> {
+    return this.http.get<User[]>(`${this.host}/user/all`);
+  }
 }
