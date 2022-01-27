96f3ce134760512adf9721c93c8a76ef6f2d8aa4
damienpuaud <damien.puaud@gmail.com>
2022-01-27 12:29:34 +0100

    CREATE - User Register Template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {UserManagementComponent} from "./user/user-management/user-management.component";
+import {RegisterComponent} from "./register/register.component";
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'user/management', component: UserManagementComponent},
+  {path: 'register', component: RegisterComponent},
   {path: '', redirectTo: '/login', pathMatch: 'full'}
 ];
 
