ded97660ec191c591a25d3e7d1e7f10a9a4564db
damienpuaud <damien.puaud@gmail.com>
2022-02-28 11:28:16 +0100

    REFACTOR - Add subscription and html structure

--- src/app/signature/signature-management/signature-management.component.ts
+++ src/app/signature/signature-management/signature-management.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {SubSink} from "subsink";
 
 @Component({
   selector: 'app-signature-management',
   templateUrl: './signature-management.component.html',
   styleUrls: ['./signature-management.component.scss']
 })
-export class SignatureManagementComponent implements OnInit {
+export class SignatureManagementComponent implements OnInit, OnDestroy {
+
+  showLoading = false;
+  private subscriptions = new SubSink();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
 }
